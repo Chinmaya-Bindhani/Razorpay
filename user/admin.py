5556468668e1b3ea7a1f061367c0fcc4e866c1b7
Chinmaya <chinmaybindhani9@gmail.com>
2026-09-23 11:56:34 +0530

    Added models into admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product,Order
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','name','price','stock']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','amount','razorpay_order_id','razorpay_payment_id','razorpay_signature','is_paid','created_at']
+    list_filter = ['is_paid','created_at']
--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.IntegerField(default=1)
